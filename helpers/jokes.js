1dc8eefc055e878df3c6c51dc8b832c2b8f93d8d
Nicole Sattler <nicole.sattler@live.com>
2018-01-05 20:38:12 -0600

    Remove jokes file

--- helpers/jokes.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-    'Why don\'t they play poker in the jungle? \n\n Too many cheetahs! :joy:',
-    'Why did the cookie go to the hospital?  \n\n Because it felt crummy!',
-    'What do you call a fake noodle? \n\n An impasta!',
-    'What do lawyers wear to court? \n\n Lawsuits!'
-]
