9d2b869849b4a6ccdd210cca0d00b2c4290d547b
Nicole Sattler <nicole.sattler@live.com>
2018-01-05 20:38:48 -0600

    Add getJoke function

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,3 +1,28 @@
+const request = require('request');
+
 exports.rand = (max) => {
     return Math.floor(Math.random() * max);
+}
+
+exports.getJoke = () => {
+    let options = {
+        url: 'https://icanhazdadjoke.com/',
+        headers: {
+            Accept: 'application/json'
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        request(options, function (error, response, body) {
+
+            if (!error && response.statusCode === 200) {
+                var data = JSON.parse(body);
+                resolve(data)
+            }
+            else {
+                reject('Something went wrong!')
+            }
+        });
+    })
+
 }
