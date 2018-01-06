6a9f3a62aed0e5d49d82259a624d31138d077c23
Nicole Sattler <nicole.sattler@live.com>
2018-01-06 11:03:01 -0600

    Modify rand function

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 exports.rand = (max) => {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max + 1);
 }
 
 exports.getJoke = () => {
